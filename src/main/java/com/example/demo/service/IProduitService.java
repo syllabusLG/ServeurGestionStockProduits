6d546efeb8d83d1fc0cc5bf13e9e0ec93e8e0d50
AbousyllabaNdiaye <AbousyllabaNdiaye@lpt-sur-aNdiaye>
2018-07-17 15:48:40 +0200

    crud produit

---
+++ src/main/java/com/example/demo/service/IProduitService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Produit;
+
+public interface IProduitService {
+
+	public List<Produit> getProduits();
+	public void addProduit(Produit produit);
+	public void updateProduit(Produit produit);
+	public void deleteProduit(String ref);
+}
