757c3203cfbe6b10894b849e3f0bb043d2df983a
AbousyllabaNdiaye <AbousyllabaNdiaye@lpt-sur-aNdiaye>
2018-07-31 11:44:33 +0200

    last comit

--- src/main/java/com/example/demo/controller/ProduitController.java
+++ src/main/java/com/example/demo/controller/ProduitController.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Produit;
 import com.example.demo.entity.User;
 
 @RestController
 @RequestMapping(value="/api/produit")
-public class ProduitController extends CrudController<User, Long>{
+public class ProduitController extends CrudController<Produit, Long>{
 
 }
